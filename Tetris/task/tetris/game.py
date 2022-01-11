10d3b7e96900eb6ab51cf4c5e1beaeefc6561b3b
parseltongist <>
2022-01-11 19:23:31 +0100

    Stage 4/4 draft

--- Tetris/task/tetris/game.py
+++ Tetris/task/tetris/game.py
@@ -1,13 +1,4 @@
 class Tetris:
-    letters_dict = {
-        "O": [[4, 14, 15, 5]],
-        "I": [[4, 14, 24, 34], [3, 4, 5, 6]],
-        "S": [[5, 4, 14, 13], [4, 14, 15, 25]],
-        "Z": [[4, 5, 15, 16], [5, 15, 14, 24]],
-        "L": [[4, 14, 24, 25], [5, 15, 14, 13], [4, 5, 15, 25], [6, 5, 4, 14]],
-        "J": [[5, 15, 25, 24], [15, 5, 4, 3], [5, 4, 14, 24], [4, 14, 15, 16]],
-        "T": [[4, 14, 24, 15], [4, 13, 14, 15], [5, 15, 25, 14], [4, 5, 6, 15]],
-    }
 
     def __init__(self, letter, m, n):
         self.letter = letter
@@ -20,6 +11,9 @@ class Tetris:
                               "bottom": [x for x in range(self.m * self.n - 1, (self.m * self.n) - self.m - 1, -1)][
                                         ::-1]
                               }
+        self.static_cells = []
+
+
 
     @property
     def letter_coordinates(self):
@@ -32,7 +26,7 @@ class Tetris:
             if _letter == "/EXIT":
                 print("Bye bye!")
                 exit()
-            elif _letter not in Tetris.letters_dict:
+            elif _letter not in 'OISZLJT':
                 print("Invalid letter, please choose among 'O' 'I' 'S' 'Z''L' 'J' 'T'")
                 return Tetris.from_string()
             return _letter
@@ -51,6 +45,22 @@ class Tetris:
 
         return cls(letter, m, n)
 
+    def reset_dict(self):
+        Tetris.letters_dict = {
+                            "O": [[4, 14, 15, 5]],
+                            "I": [[4, 14, 24, 34], [3, 4, 5, 6]],
+                            "S": [[5, 4, 14, 13], [4, 14, 15, 25]],
+                            "Z": [[4, 5, 15, 16], [5, 15, 14, 24]],
+                            "L": [[4, 14, 24, 25], [5, 15, 14, 13], [4, 5, 15, 25], [6, 5, 4, 14]],
+                            "J": [[5, 15, 25, 24], [15, 5, 4, 3], [5, 4, 14, 24], [4, 14, 15, 16]],
+                            "T": [[4, 14, 24, 15], [4, 13, 14, 15], [5, 15, 25, 14], [4, 5, 6, 15]],
+        }
+
+    def place_first_letter(self):
+        self.fill_out_grid()
+        self.print_grid()
+        self.reset_grid()
+
     def print_grid(self):
         i = 0
         for _ in range(self.n):
@@ -58,9 +68,13 @@ class Tetris:
             i += self.m
         print()
 
-    def fill_out_grid(self, rotation_variant):
+    def fill_out_grid(self, ):
+        if self.static_cells:
+            for cell in self.static_cells:
+                self.grid[cell] = "0"
+
         # use generator?:
-        for y in self.letters_dict[self.letter][rotation_variant]:
+        for y in self.letters_dict[self.letter][self.rotation]:
             try:
                 self.grid[y] = "0"
             except IndexError:
@@ -92,14 +106,37 @@ class Tetris:
 
     def is_valid_move(self, direction=None):
         for coordinate in self.letter_coordinates:
-            if coordinate in self.border_points[direction] or coordinate in self.border_points["bottom"]:
+            if coordinate in self.border_points[direction] or coordinate in self.static_cells or coordinate in self.border_points["bottom"]:
                 return False
         return True
 
-    def place_first_letter(self):
-        self.fill_out_grid(self.rotation)
-        self.print_grid()
-        self.reset_grid()
+    def make_static(self):
+        # or coodinate is ON static cell. - make it static
+        static = False
+        for coordinate in self.letter_coordinates:
+            if coordinate in self.border_points["bottom"] or coordinate in [x-self.m for x in self.static_cells]:
+                static = True
+                break
+
+        print(f"Static now is:{static}")
+        if static:
+            for coordinate in self.letter_coordinates:
+                self.static_cells.append(coordinate)
+            # self.letter = choice(list(self.letters_dict.keys()))
+
+
+    def game_over(self):
+        return  "Full grid condition"
+
+    def drop_row(self):
+        control_row = ["-" for _ in range(self.m)]
+        last_index = self.m * self.n
+        l1 = last_index - self.m
+        bottom_row = self.grid[last_index:l1:-1]
+
+        print(bottom_row==last_index)  # IT WORKS!
+
+
 
     def move_it(self):
         while True:
@@ -117,19 +154,40 @@ class Tetris:
             elif action in {"left", "right"}:
                 self.move(action)
 
+            elif action == "break":
+                self.fill_out_grid()  # for test only
+                self.drop_row()
+                self.reset_grid()  # for test only
+
+            elif action == "piece":
+                self.reset_dict()
+                self.letter = input().strip().upper()
+                self.rotation = 0
+                print("LETTER ADDED", self.letter)
+                print("Current letter coordinates:", self.letter_coordinates)
+                self.place_first_letter()
+                return self.move_it()
+
+                # self.print_grid()
+                # the bug is that after all manipulations with the piece, it's final coordinates will be in static_moves. It makes sence to reset original letters coordinates
+
+
             else:
                 print("Invalid option. Try any of the following options\n'left', 'right', 'down', 'rotate', 'exit': ")
                 return self.move_it()
 
             # down - anyway
             self.move("down")
-            self.fill_out_grid(self.rotation)
+            self.make_static()
+            self.fill_out_grid()
             self.print_grid()
             self.reset_grid()
 
 
+
 def main():
     game = Tetris.from_string()
+    Tetris.reset_dict(game)
     game.print_grid()
     game.place_first_letter()
     game.move_it()
