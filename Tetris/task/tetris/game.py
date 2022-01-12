ac97c72458e4b4bb85a190aef4c878f275dbd67a
parseltongist <>
2022-01-12 20:20:26 +0100

    Stage 4/4 very bad quality but working.

--- Tetris/task/tetris/game.py
+++ Tetris/task/tetris/game.py
@@ -14,6 +14,17 @@ class Tetris:
                               }
         self.static_cells = set()
 
+    def reset_dict(self):
+        self.letters_dict = {
+            "O": [[4, 14, 15, 5]],
+            "I": [[4, 14, 24, 34], [3, 4, 5, 6]],
+            "S": [[5, 4, 14, 13], [4, 14, 15, 25]],
+            "Z": [[4, 5, 15, 16], [5, 15, 14, 24]],
+            "L": [[4, 14, 24, 25], [5, 15, 14, 13], [4, 5, 15, 25], [6, 5, 4, 14]],
+            "J": [[5, 15, 25, 24], [15, 5, 4, 3], [5, 4, 14, 24], [4, 14, 15, 16]],
+            "T": [[4, 14, 24, 15], [4, 13, 14, 15], [5, 15, 25, 14], [4, 5, 6, 15]],
+        }
+
     @property
     def letter_coordinates(self):
         return self.letters_dict[self.letter][self.rotation]
@@ -33,28 +44,17 @@ class Tetris:
 
         return cls(m, n)
 
-    def reset_dict(self):
-        self.letters_dict = {
-            "O": [[4, 14, 15, 5]],
-            "I": [[4, 14, 24, 34], [3, 4, 5, 6]],
-            "S": [[5, 4, 14, 13], [4, 14, 15, 25]],
-            "Z": [[4, 5, 15, 16], [5, 15, 14, 24]],
-            "L": [[4, 14, 24, 25], [5, 15, 14, 13], [4, 5, 15, 25], [6, 5, 4, 14]],
-            "J": [[5, 15, 25, 24], [15, 5, 4, 3], [5, 4, 14, 24], [4, 14, 15, 16]],
-            "T": [[4, 14, 24, 15], [4, 13, 14, 15], [5, 15, 25, 14], [4, 5, 6, 15]],
-        }
-
-    def place_first_letter(self):
-        self.fill_out_grid()
-        self.print_grid()
-        self.reset_grid()
-
     def print_grid(self):
         i = 0
         for _ in range(self.n):
             print(" ".join(self.grid[i:i + self.m]))
             i += self.m
         print()
+
+    def place_first_letter(self):
+        self.fill_out_grid()
+        self.print_grid()
+        self.reset_grid()
 
     def fill_out_grid(self, ):
         if self.static_cells:
@@ -73,14 +73,14 @@ class Tetris:
 
     def rotate(self):
         # creating back up
-        current_rotation = self.rotation
+
         # rotate
         self.rotation += 1 if (
                 self.rotation + 1 <= len(self.letters_dict[self.letter]) - 1 or self.rotation == -1) else 0
         # validate
         if not all([self.is_valid_move(direction="left"), self.is_valid_move(direction="right")]):
             # undo if validation failed
-            self.rotation = current_rotation
+            self.rotation -= 1
 
     def move(self, direction):  # it was left
         direction = "bottom" if direction == "down" else direction
@@ -95,8 +95,8 @@ class Tetris:
 
     def is_valid_move(self, direction=None):
         for coordinate in self.letter_coordinates:
-            if coordinate in self.border_points[direction] or coordinate in self.static_cells or coordinate in \
-                    self.border_points["bottom"]:
+            if coordinate in self.border_points[direction] or coordinate in self.static_cells \
+                    or coordinate in self.border_points["bottom"]:
                 return False
         return True
 
@@ -104,21 +104,16 @@ class Tetris:
         # or coodinate is ON static cell. - make it static
         static = False
         if self.letter:
-            for coordinate in self.letter_coordinates:
-                if coordinate in self.border_points["bottom"] or coordinate in [x - self.m for x in self.static_cells]:
-                    static = True
-                    break
+            in_border_points = lambda x: x in self.border_points["bottom"] or x in [x - self.m for x in self.static_cells]
 
-        # print(f"Static now is:{static}")
-        if static:
-            for coordinate in self.letter_coordinates:
-                self.static_cells.add(coordinate)
-
-            self.letter = None  # need to clear letter coordinates once it became static
+            hit_the_border = [coordinate for coordinate in self.letter_coordinates if in_border_points(coordinate)]
+            if hit_the_border:
+                for coordinate in self.letter_coordinates:
+                    self.static_cells.add(coordinate)
+                self.letter = None  # need to clear letter coordinates once it became static
 
     def game_end_check(self):
         # print("currently the following cells are static:", self.static_cells)
-
         for cell in self.static_cells:
             if cell in [x for x in range(self.m)]:
                 print("Game Over!")
@@ -133,10 +128,8 @@ class Tetris:
 
             self.static_cells = set(self.static_cells)
 
-
         original_static_capacity = len(self.static_cells)
         control_row = ["0" for _ in range(self.m)]
-
 
         i = 0
         for _ in range(self.n):
@@ -151,45 +144,40 @@ class Tetris:
         if original_static_capacity != len(self.static_cells):
             recalculate_static_cells()
 
-
-
     def move_it(self):
         while True:
-
-            # add match case?
             action = input("\n")
-            if action == "exit":
-                exit()
+            match action:
 
-            elif action == "rotate":
-                self.rotate()
+                case "exit":
+                    exit()
 
-            elif action == "down":
-                pass
+                case "rotate":
+                    self.rotate()
 
-            elif action in {"left", "right"}:
-                self.move(action)
+                case "down":
+                    pass
 
-            elif action == "break":
-                self.fill_out_grid()  # for test only
-                self.drop_row()
-                self.reset_grid()  # for test only
+                case "left" | "right":  # "|" == "or"
+                    self.move(action)
 
-            elif action == "piece":
-                self.reset_dict()
-                self.letter = input().strip().upper()
-                self.rotation = 0
-                # print("LETTER ADDED", self.letter)
-                # print("Current letter coordinates:", self.letter_coordinates)
-                self.place_first_letter()
-                self.make_static()
-                # self.game_end_check()
+                case "break":
+                    self.fill_out_grid()  # for test only
+                    self.drop_row()
+                    self.reset_grid()  # for test only
 
-                return self.move_it()
+                case "piece":
+                    self.reset_dict()
+                    self.letter = input().strip().upper()
+                    self.rotation = 0
 
-            else:
-                print("Invalid option. Try any of the following options\n'left', 'right', 'down', 'rotate', 'exit': ")
-                return self.move_it()
+                    self.place_first_letter()
+                    self.make_static()
+                    return self.move_it()
+
+                case _:
+                    print("Invalid option.\nTry any of the following options\n'left', 'right', 'down', 'rotate', 'exit': ")
+                    return self.move_it()
 
             # down - anyway
             if self.letter:
